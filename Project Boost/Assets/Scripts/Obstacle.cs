03754e364faa971a30829cf9064f83754dba96c5
Andrew Huntington <andrew.huntington@gmail.com>
2019-07-15 23:46:05 +0900

    15_PB_CU2 Basic Level Design
    -Added a background
    -Laid out a basic level
    -Created an Obstacle.cs script that moves objects vertically

---
+++ Project Boost/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{    
+    // this code is a clusterfuck. maybe clean up some day...
+    Vector3 obstaclePostion;
+    [SerializeField] float startingYValue = 29f;
+    float currentXPos;
+    float currentYPos;
+    float currentZPos;
+    bool complete = false;
+    [SerializeField] float moveRate = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentXPos = transform.position.x;
+        currentYPos = startingYValue;
+        currentZPos = transform.position.z;
+        obstaclePostion = new Vector3(currentXPos, currentYPos, currentZPos);
+        transform.position = obstaclePostion;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentYPos <= startingYValue && !complete)
+        {
+            DownMovement();
+            if (currentYPos <= 15)
+            {
+                complete = true;
+            }
+        } else if (complete)
+        {
+            UpMovement();
+            if (currentYPos >= startingYValue)
+            {
+                complete = false;
+            }
+        }   
+    }
+
+    private void UpMovement()
+    {
+        obstaclePostion = new Vector3(currentXPos, (currentYPos += moveRate), currentZPos);
+        transform.position = obstaclePostion;
+    }
+
+    private void DownMovement()
+    {
+        obstaclePostion = new Vector3(currentXPos, (currentYPos -= moveRate), currentZPos);
+        transform.position = obstaclePostion;
+
+    }
+    
+}
