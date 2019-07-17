25915b295ce2daad460d9f440f8b56f4707d86e6
Andrew Huntington <andrew.huntington@gmail.com>
2019-07-17 23:06:22 +0900

    Improved MoveObstacle.cs

--- Project Boost/Assets/Scripts/MoveObstacle.cs
+++ Project Boost/Assets/Scripts/MoveObstacle.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class MoveObstacle : MonoBehaviour
 {
-    // this code is a clusterfuck. maybe clean up some day...
     Vector3 obstaclePostion;
-    [SerializeField] bool turnOn;
-    [SerializeField] float startingYValue = 29f;
+
+    float startXPos;
+    float startYPos;
+    float startZPos;
+
+    float maxXpos;
+    float minXpos;
+    float maxYpos;
+    float minYpos;
+    float maxZpos;
+    float minZpos;
+
     float currentXPos;
     float currentYPos;
     float currentZPos;
-    bool complete = false;
-    [SerializeField] float moveRate = 0.5f;
+    bool complete;
+
+    [SerializeField] bool turnOn;
+    [SerializeField] float adjustYUp = 0f; // max obstacle height from starting position
+    [SerializeField] float adjustYDown = 0f; // min obstacle height from starting position
+    [SerializeField] float moveRate = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentXPos = transform.position.x;
-        currentYPos = startingYValue;
-        currentZPos = transform.position.z;
+        startXPos = currentXPos = transform.position.x;
+        startYPos = currentYPos = transform.position.y;
+        startZPos = currentZPos = transform.position.z;
+        maxYpos = startYPos + adjustYUp;
+        minYpos = startYPos - adjustYDown;
         obstaclePostion = new Vector3(currentXPos, currentYPos, currentZPos);
         transform.position = obstaclePostion;
     }
@@ -27,38 +42,41 @@ public class MoveObstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        while (turnOn)
+        if (turnOn)
         {
-            if (currentYPos <= startingYValue && !complete)
+            moveYAxis();
+        }
+    }
+
+    private void moveYAxis()
+    {        
+        if (currentYPos < maxYpos && !complete)
+        {
+            upMovement();
+            if (currentYPos >= maxYpos)
             {
-                DownMovement();
-                if (currentYPos <= 15)
-                {
-                    complete = true;
-                }
-            }
-            else if (complete)
-            {
-                UpMovement();
-                if (currentYPos >= startingYValue)
-                {
-                    complete = false;
-                }
+                complete = true;
             }
         }
-        
+        else if (complete)
+        {
+            downMovement();
+            if (currentYPos <= minYpos)
+            {
+                complete = false;
+            }
+        }
     }
 
-    private void UpMovement()
+    private void upMovement()
     {
-        obstaclePostion = new Vector3(currentXPos, (currentYPos += moveRate), currentZPos);
+        obstaclePostion = new Vector3(currentXPos, (currentYPos += (moveRate * Time.deltaTime)), currentZPos);
         transform.position = obstaclePostion;
     }
 
-    private void DownMovement()
+    private void downMovement()
     {
-        obstaclePostion = new Vector3(currentXPos, (currentYPos -= moveRate), currentZPos);
+        obstaclePostion = new Vector3(currentXPos, (currentYPos -= (moveRate * Time.deltaTime)), currentZPos);
         transform.position = obstaclePostion;
-
     }
 }
